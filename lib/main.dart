5dffb9fa9f57050f320fdda55e9181aceae12e24
xAHRx <alanhoskingrivera@gmail.com>
2022-01-14 15:54:22 -0400

    Added datee example & Add task

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,11 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter/material.dart';
 
 class Todos {
-  Todos({required this.name, required this.checked});
+  Todos({
+    required this.name,
+  });
   final String name;
-  bool checked;
 }
-
-// class TodoItem extends StatelessWidget {
-//  TodoItem({
-//     required this.todos,
-//     required this.onTodoChanged,
-//   }) : super(key: ObjectKey(todos));
-
-//   final Todos todos;
-//   final onTodoChanged;
-// }
 
 void main() => runApp(const TodoApp());
 
@@ -50,9 +41,9 @@ class MyStatefulWidget extends StatefulWidget {
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController myController = TextEditingController();
-  final List<Todos> _todos = <Todos>[];
-
+  TextEditingController myController = TextEditingController();
+  List<Todos> todos = <Todos>[];
+  DateTime date = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -82,27 +73,77 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               return null;
             },
           ),
+          Text(
+            "Insert Due Date",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w900,
+              fontFamily: "Georgia",
+            ),
+          ),
+          ElevatedButton(
+              child: Text('select date'), onPressed: selectTimePicker(context)),
+          Text(date.day.toString()),
+          Text(date.month.toString()),
+          Text(date.year.toString()),
           Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
-                  child: const Text('Submit'),
-                  onPressed: () => Text(this.myController.toString())
-                  // Validate returns true if the form is valid, or false otherwise.
-                  ))
+                child: const Text('Submit'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _addTodoItem();
+                  }
+                },
+                // Validate returns true if the form is valid, or false otherwise.
+              )),
+          Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: todos.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 50,
+                      margin: EdgeInsets.all(2),
+                      child: Center(
+                          child: Text(
+                        '${todos[index]}',
+                        style: TextStyle(fontSize: 18),
+                      )),
+                    );
+                  }))
         ],
       ),
     );
   }
 
-  void _addTodoItem(String name) {
-    setState(() {
-      if (_formKey.currentState!.validate()) {
-        // If the form is valid, display a snackbar. In the real world,
-        // you'd often call a server or save the information in a database.
+  selectTimePicker(BuildContext context) {
+    Future<Null> selectTimePicker(BuildContext context) async {
+      final DateTime? picked = await showDatePicker(
+          context: context,
+          initialDate: date,
+          firstDate: DateTime(1940),
+          lastDate: DateTime(2030));
+      if (picked != null && picked != date) {
+        setState(() {
+          date = picked;
+          print(date.toString());
+        });
       }
-      myController.value;
-      //_todos.add(Todos(name: name, checked: false));
-    });
-    myController.clear();
+    }
+  }
+
+  void _addTodoItem() {
+    setState(() {
+      todos.add(Todos(name: myController.text));
+      todos.map((todos) => Todos(name: myController.text)).toList();
+      //todos.insert(0,myController.value);
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+    }
+
+        //_todos.add(Todos(name: name, checked: false));
+
+        );
   }
 }
